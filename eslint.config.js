84aa825b20b758e167d01d15fc65d7daedac7822
Andrii Kaleniuk <and.kaleniuk@gmail.com>
2026-08-15 23:54:44 +0300

    feat: dummy structure form

--- eslint.config.js
+++ eslint.config.js
@@ -1,76 +1,82 @@
-import js from '@eslint/js';
-import prettierConfig from 'eslint-config-prettier';
-import importPlugin from 'eslint-plugin-import';
-import prettierPlugin from 'eslint-plugin-prettier';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import globals from 'globals';
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import prettierPlugin from "eslint-plugin-prettier";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 
 export default [
   // ігнорування збірки
-  { ignores: ['dist', 'node_modules'] },
+  { ignores: ["dist", "node_modules"] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       globals: globals.browser,
       parserOptions: {
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
     plugins: {
       import: importPlugin,
       react: reactPlugin,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'simple-import-sort': simpleImportSort,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "simple-import-sort": simpleImportSort,
       prettier: prettierPlugin,
     },
     rules: {
-      // базові правила
+      // базові
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       ...prettierConfig.rules,
 
-      // налаштування форматування
-      'prettier/prettier': ['error', {
-        singleQuote: true,
-        semi: true,
-      }],
+      // форматування
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: true,
+          semi: true,
+        },
+      ],
 
-      // правила викладача
-      'no-unused-vars': 'warn',
-      'no-undef': 'error',
-      'eqeqeq': 'error',
-      'no-console': 'warn',
-      'prefer-const': 'error',
-      'no-var': 'error',
-      'object-shorthand': 'error',
-      'import/no-unused-modules': 'warn',
-      'react/jsx-no-useless-fragment': 'warn',
-      'react/no-array-index-key': 'error',
-      'react/jsx-key': 'error',
-      'react-hooks/exhaustive-deps': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/set-state-in-effect': 'off',
+      // для команди
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      eqeqeq: "error",
+      "no-console": "warn",
+      "prefer-const": "error",
+      "no-var": "error",
+      "object-shorthand": "error",
+      "import/no-unused-modules": "warn",
+      "react/jsx-no-useless-fragment": "warn",
+      "react/no-array-index-key": "error",
+      "react/jsx-key": "error",
+      "react-hooks/exhaustive-deps": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/set-state-in-effect": "off",
 
       // сортування імпортів
-      'simple-import-sort/imports': ['warn', {
-        groups: [
-          ["^\\u0000"],
-          ["^node:"],
-          ["^react$", "^react-dom$"],
-          ["^@?\\w"],
-          ["^@/"],
-          ["^~/" ],
-          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
-          ["^\\./(?=.*/)", "^\\.(?!/?$)", "^\\./?$"],
-          ["^.+\\.s?css$"],
-        ],
-      }],
-      'simple-import-sort/exports': 'off',
+      "simple-import-sort/imports": [
+        "warn",
+        {
+          groups: [
+            ["^\\u0000"],
+            ["^node:"],
+            ["^react$", "^react-dom$"],
+            ["^@?\\w"],
+            ["^@/"],
+            ["^~/"],
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            ["^\\./(?=.*/)", "^\\.(?!/?$)", "^\\./?$"],
+            ["^.+\\.s?css$"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "off",
     },
-  }
+  },
 ];
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import Header from "./components/Header/Header.jsx";
+import ContactList from "./components/ContactList/ContactList.jsx";
+import ContactForm from "./components/ContactForm/ContactForm.jsx";
+import styles from "./App.module.css";
+
 function App() {
   return (
-      <div>
-
+    <div className="appContainer">
+      <Header />
+      <div className={styles.contentWrapper}>
+        <ContactList />
+        <ContactForm />
       </div>
+    </div>
   );
 }
 
